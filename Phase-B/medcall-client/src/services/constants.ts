a07425170624a22e48d11fe084725170393d682e
Tareez Ghandour <tareezghandour@hotmail.com>
2024-06-30 18:37:31 +0300

    fix yaml google auth

--- Phase-B/medcall-client/src/services/constants.ts
+++ Phase-B/medcall-client/src/services/constants.ts
@@ -1,4 +1,5 @@
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
+console.log("🚀 ~ apiUrl:", apiUrl);
 
 export const loginUrl = `${apiUrl}/users/login`;
 export const registerUrl = `${apiUrl}/users/register`;
