449c692a6b4753747ebd1e6fc43e93e1758489f9
Tareez Ghandour <tareezghandour@hotmail.com>
2024-07-05 12:11:52 +0300

    remove cors options

--- Phase-B/medcall-server/functions/src/index.ts
+++ Phase-B/medcall-server/functions/src/index.ts
@@ -6,33 +6,18 @@ const dotenv = require('dotenv');
 import routes from "./routes";
 import { connectDb } from "./db/index";
 
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 const env = functions.config().NODE_ENV || 'development';
 dotenv.config({ path: `.env.${env}` });
-const corsOptions = {
-  origin: "*",
-  credentials: true, // This is important for setting the Access-Control-Allow-Credentials header
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-};
+
+const firebaseConfig = functions.config().firebase;
+console.log(firebaseConfig);
 
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
-});
-
-app.options('*', cors(corsOptions));
+app.use(cors());
 app.use(routes);
 
 const PORT = process.env.PORT;
