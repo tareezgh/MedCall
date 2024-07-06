d52c3931162738576f1c624cc751097428868d50
Tareez Ghandour <tareezghandour@hotmail.com>
2024-07-06 11:36:54 +0300

    add middleware

--- Phase-B/medcall-server/functions/src/index.ts
+++ Phase-B/medcall-server/functions/src/index.ts
@@ -1,37 +1,32 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const dotenv = require("dotenv");
 import routes from "./routes";
 import { connectDb } from "./db/index";
-
 import { Request, Response } from "express";
+import { applyMiddleware } from "./middleware/middleware";
 
 dotenv.config({ path: `.env.production` });
 
 const app = express();
-
-const corsOptions = {
-  origin: 'https://medcall-client.web.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204
-};
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors(corsOptions));
+applyMiddleware(app);
 app.use(routes);
-
-const PORT = process.env.PORT;
 
 app.get("/some-data", (request: Request, response: Response) => {
   response.send("Hello world");
 });
 
 connectDb().then(async () => {
-  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+  console.log(`Connected to the database`);
 });
 
-exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest((req: Request, res: Response) => {
+  res.set("Access-Control-Allow-Origin", "https://medcall-client.web.app");
+  res.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  app(req, res);
+});
