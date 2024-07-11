506dbee840647ff6b6ff0b2f35c464a4deaa6255
leen7awa <leen.hawa@e.braude.ac.il>
2024-07-11 10:28:10 +0300

    fix: user-dashboard created

--- Phase-B/medcall-client/src/components/DashboardContent.tsx
+++ Phase-B/medcall-client/src/components/DashboardContent.tsx
@@ -1,8 +1,11 @@
 import { useSelector } from "react-redux";
+import Button from "../components/Button";
+// TODO: the plus icon for 'New Request'
+// import { PlusIcon } from "../components/icons";
 
-interface DashboardContentProps {}
+interface DashboardContentProps { }
 
-const DashboardContent = ({}: DashboardContentProps) => {
+const DashboardContent = ({ }: DashboardContentProps) => {
   const currentUser = useSelector((state: any) => state.currentUser);
   const getWidths = () => {
     switch (currentUser.role) {
@@ -18,27 +21,84 @@ const DashboardContent = ({}: DashboardContentProps) => {
   };
 
   const { leftWidth, rightWidth } = getWidths();
+
+  const noActiveRequest = () => {
+    return (
+      <>
+      </>
+    );
+  };
+
+  const activeRequest = () => {
+    return (
+      <>
+        <section className="flex justify-center items-center">
+          <div className="flex py-8 justify-start text-center flex-col gap-8 bg-modalBackground rounded-2xl w-full h-fit min-h-[250px] shadow-xl">
+            <h2 className="text-3xl font-bold">Active Requests</h2>
+            <div className="flex flex-col justify-center items-center gap-4">
+              <h5 className="text-xl">The ambulance in his way, you can track it's route.</h5>
+              {/* //TODO print accident type 
+              */}
+              <h5 className="text-xl font-bold">Accident</h5>
+              <Button
+                text='Track Now'
+                type="primary"
+                onClick={() => { }}
+                customClassName="text-2xl bg-green-600 hover:bg-green-700 text-white"
+              />
+            </div>
+          </div>
+        </section></>
+    );
+  };
+
   return (
-    <div className="flex flex-col items-start w-full gap-4">
-      <h1 className="text-4xl w-full text-start">
-        Welcome {`${currentUser.firstName}`}
-      </h1>
-      <div className="flex flex-row gap-4 w-full h-full">
-        <div
-          className="left-side"
-          style={{ width: `${leftWidth}%`, background: "#f0f0f0" }}
-        >
-          Left Side Content
-        </div>
-        <div
-          className="right-side"
-          style={{ width: `${rightWidth}%`, background: "#d0d0d0" }}
-        >
-          Right Side Content
+    <>
+      <div className="flex flex-col items-start w-full gap-4">
+        <h1 className="text-4xl w-full text-start">
+          Welcome {`${currentUser.firstName}`}
+        </h1>
+        <div className="flex flex-row gap-4 w-full h-full">
+          <div
+            className="left-side"
+            style={{ width: `${leftWidth}%`, background: "#f0f0f0" }}
+          >
+            {/* check condition here */}
+            {noActiveRequest()}
+
+            <section className="py-4 flex justify-center items-center">
+              <div className="flex py-8 justify-start text-center flex-col gap-8 bg-modalBackground rounded-2xl w-full h-fit min-h-[250px] shadow-xl">
+                <h2 className="text-3xl font-bold">Request New Ambulance</h2>
+                <div className="flex flex-col justify-center items-center gap-4">
+                  <h5 className="text-xl">Request new ambulance for your<br />emergency and save life</h5>
+                  <Button
+                    text='Order Now'
+                    type="primary"
+                    onClick={() => { }}
+                    customClassName="text-2xl bg-red-700 hover:bg-red-800 text-white"
+                  />
+                </div>
+              </div>
+            </section>
+
+          </div>
+          <div
+            className="right-side"
+            style={{ width: `${rightWidth}%` }}
+          >
+            {/* Right Side Content */}
+            <section className="flex justify-center items-center">
+              <div className="flex py-8 justify-start text-start flex-col gap-8 bg-modalBackground rounded-2xl w-full h-fit min-h-[554px] shadow-xl">
+                <h2 className="text-3xl font-bold ml-[10px]">History</h2>
+              </div>
+            </section>
+
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
+
 };
 
 export default DashboardContent;
